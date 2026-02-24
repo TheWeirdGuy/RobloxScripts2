d094e6d6f0cfe9da8c9b4ac96d3870930178ea8f
C0d3F4nt0m <aruschmeyer8@gmail.com>
2026-02-24 18:17:43 +0100

    Implement player settings UI with speed and jump sliders
    Added a library for UI and implemented sliders for speed and jump settings.

--- test.lua
+++ test.lua
@@ -1,1 +1,37 @@
-print("Hello")
+local Library = loadstring(game:HttpGet('https://raw.githubusercontent.com/Rain-Design/Unnamed/main/Library.lua'))()
+Library.Theme = "Dark"
+local Flags = Library.Flags
+
+local Window = Library:Window({
+    Text = "Baseplate"
+})
+
+local Tab = Window:Tab({
+    Text = "Main"
+})
+
+local Section = Tab:Section({
+    Text = "Player Settings"
+})
+
+Section:Slider({
+    Text = "Speed",
+    Default = 16,
+    Minimum = 0,
+    Maximum = 200,
+    Callback = function(value)
+        game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = value
+    end
+})
+
+Section:Slider({
+    Text = "Jump",
+    Default = 50,
+    Minimum = 0,
+    Maximum = 200,
+    Callback = function(value)
+        game.Players.LocalPlayer.Character.Humanoid.JumpPower = value
+    end
+})
+
+Tab:Select()
